96f614530462f9b0e6aac936dac83da45da49b45
guangguangguang1 <56507043+guangguangguang1@users.noreply.github.com>
2020-01-05 16:03:51 +0800

    界面完善

--- CodePrinter/CodePrinterDlg.cpp
+++ CodePrinter/CodePrinterDlg.cpp
@@ -1865,27 +1865,39 @@ void CCodePrinterDlg::OnTimer(UINT_PTR nIDEvent)
 
 HBRUSH CCodePrinterDlg::OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor)
 {
+	//HBRUSH hbr = CDialog::OnCtlColor(pDC, pWnd, nCtlColor);
+
+	//// TODO:  在此更改 DC 的任何属性
+ //  /* if(nCtlColor == CTLCOLOR_STATIC)
+	//{
+	//	switch(pWnd->GetDlgCtrlID())
+	//	{			
+	//		case IDC_STATIC_SHOW_DLG:
+	//		{
+	//			pDC->SelectObject(theApp.m_HeadOperationStaFont);
+	//			pDC->SetBkMode(TRANSPARENT);
+	//			pDC->SetTextColor(RGB(255,255,255));
+	//			break;
+	//		}
+	//		default:
+	//			break;
+	//	}
+	//} */
+
+	//pDC->SetBkColor(theApp.m_BKcolor);
+	// 
+	//return theApp.m_DlgBrush;
+
 	HBRUSH hbr = CDialog::OnCtlColor(pDC, pWnd, nCtlColor);
-
+	if(nCtlColor == CTLCOLOR_STATIC)
+	{		 
+		pDC->SelectObject(theApp.m_StaticFont);
+		pDC->SetBkMode(TRANSPARENT);
+		pDC->SetTextColor(RGB(0,0,0));	
+	}
 	// TODO:  在此更改 DC 的任何属性
-   /* if(nCtlColor == CTLCOLOR_STATIC)
-	{
-		switch(pWnd->GetDlgCtrlID())
-		{			
-			case IDC_STATIC_SHOW_DLG:
-			{
-				pDC->SelectObject(theApp.m_HeadOperationStaFont);
-				pDC->SetBkMode(TRANSPARENT);
-				pDC->SetTextColor(RGB(255,255,255));
-				break;
-			}
-			default:
-				break;
-		}
-	} */
-
-	pDC->SetBkColor(theApp.m_BKcolor);
-	 
+	pDC->SetBkColor(theApp.m_BKcolor);	
+	// TODO:  如果默认的不是所需画笔，则返回另一个画笔
 	return theApp.m_DlgBrush;
 }
 
